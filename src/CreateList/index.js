2e68650727b491299dd85173d4b8f96a5eab5f06
Christine Samarchi <csamarchi@gmail.com>
2019-01-07 16:09:39 -0600

    created login, landing, and create list page components

---
+++ src/CreateList/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class CreateList extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default CreateList;
