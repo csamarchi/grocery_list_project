d9f3bf662f96d26e02221e2f5b8d391699985171
Christine Samarchi <csamarchi@gmail.com>
2019-01-07 16:36:30 -0600

    bad navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1> Hi! </h1>
+        <NavBar />
       </div>
     );
   }
