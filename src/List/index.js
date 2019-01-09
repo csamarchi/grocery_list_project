10ce1894553a340ff2d93442e3bf20984df48ea4
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2019-01-09 12:54:34 -0600

    added routes for incrementing grocery list

--- src/List/index.js
+++ src/List/index.js
@@ -3,16 +3,58 @@ import NavBar from '../NavBar';
 import './style.css';
 
 class List extends Component {
-  constructor() {
-    super();
+
+  constructor(props) {
+    super(props);
+    this.state={
+      category: '',
+      name: '',
+      _id: this.props.data._id
+    }
   }
 
+
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const addItem = await fetch('http://localhost:9000/addItem', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(this.state),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }
   render() {
+    console.log(this.props.data._id, 'COFFEEE')
     return(
       <div className='background'>
           <h1> {this.props.data.name} </h1>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input type='text' name='name' placeholder='your item..' onChange={this.handleChange}/>
+            <select name='category' onChange={this.handleChange}>
+              <option value='produce'>Produce</option>
+              <option value='meats'>Meats</option>
+              <option value='deli'>Deli</option>
+              <option value='dairy'>Dairy</option>
+              <option value='bakery'>Bakery</option>
+              <option value='frozen'>Frozen</option>
+              <option value='dryGoods'>Dry Goods</option>
+              <option value='drinks'>Drinks</option>
+              <option value='alcohol'>Alcohol</option>
+              <option value='general'>General</option>
+            </select>
             <input type='Submit' value='+'/>
           </form>
           <div className='categoryWrapper'>
