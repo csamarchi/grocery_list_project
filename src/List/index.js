56ca9b3a91d53639fa193430e48f52b3814b1a98
Christine Samarchi <csamarchi@gmail.com>
2019-01-15 10:56:43 -0600

    added style edits to the add item form

--- src/List/index.js
+++ src/List/index.js
@@ -71,73 +71,70 @@ class List extends Component {
 
     return(
       <div className='background'>
-
-
-          <h1> {this.props.data.name} </h1>
-
-          <form onSubmit={this.handleSubmit}>
-            <input type='text' name='name' placeholder='your item..' onChange={this.handleChange}/>
-            <select name='category' onChange={this.handleChange}>
-              <option value='produce'>Produce</option>
-              <option value='meats'>Meats</option>
-              <option value='deli'>Deli</option>
-              <option value='dairy'>Dairy</option>
-              <option value='bakery'>Bakery</option>
-              <option value='frozen'>Frozen</option>
-              <option value='dryGoods'>Dry Goods</option>
-              <option value='drinks'>Drinks</option>
-              <option value='alcohol'>Alcohol</option>
-              <option value='general'>General</option>
-            </select>
-            <input type='Submit' value='+'/>
-          </form>
-
-
-
-          <div className='categoryWrapper'>
-              <div className='category'>
-                <h1> Produce </h1>
-                {this.state.list.produce ?  this.state.list.produce.map((item) => <p>{item}</p>) : null}
+          <h1 className='listName'> {this.props.data.name} </h1>
+          <div className='wrapper'>
+            <form onSubmit={this.handleSubmit}>
+              <input className='addItemInput'type='text' name='name' placeholder='your item..' onChange={this.handleChange}/>
+              <div className="select">
+                <select name='category' onChange={this.handleChange}>
+                  <option> Choose a category </option>
+                  <option value='produce'>Produce</option>
+                  <option value='meats'>Meats</option>
+                  <option value='deli'>Deli</option>
+                  <option value='dairy'>Dairy</option>
+                  <option value='bakery'>Bakery</option>
+                  <option value='frozen'>Frozen</option>
+                  <option value='dryGoods'>Dry Goods</option>
+                  <option value='drinks'>Drinks</option>
+                  <option value='alcohol'>Alcohol</option>
+                  <option value='general'>General</option>
+                </select>
               </div>
-              <div className='category'>
-                <h1> Meats </h1>
-                {this.state.list.meats ?  this.state.list.meats.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> Deli </h1>
-                {this.state.list.deli ?  this.state.list.deli.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> Dairy </h1>
-                {this.state.list.dairy ?  this.state.list.dairy.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> Bakery </h1>
-                {this.state.list.bakery ?  this.state.list.bakery.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> Frozen </h1>
-                {this.state.list.frozen ?  this.state.list.frozen.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> Dry Goods </h1>
-                {this.state.list.dryGoods ?  this.state.list.dryGoods.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> Drinks </h1>
-                {this.state.list.drinks ?  this.state.list.drinks.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> Alcohol </h1>
-                {this.state.list.alcohol ?  this.state.list.alcohol.map((item) => <p>{item}</p>) : null}
-              </div>
-              <div className='category'>
-                <h1> General </h1>
-                {this.state.list.general ?  this.state.list.general.map((item) => <p>{item}</p>) : null}
-              </div>
+              <input className='addItemButton' type='Submit' value='+'/>
+            </form>
           </div>
-
-
+            <div className='categoryWrapper'>
+                <div className='category'>
+                  <h1> Produce </h1>
+                  {this.state.list.produce ?  this.state.list.produce.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Meats </h1>
+                  {this.state.list.meats ?  this.state.list.meats.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Deli </h1>
+                  {this.state.list.deli ?  this.state.list.deli.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Dairy </h1>
+                  {this.state.list.dairy ?  this.state.list.dairy.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Bakery </h1>
+                  {this.state.list.bakery ?  this.state.list.bakery.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Frozen </h1>
+                  {this.state.list.frozen ?  this.state.list.frozen.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Dry Goods </h1>
+                  {this.state.list.dryGoods ?  this.state.list.dryGoods.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Drinks </h1>
+                  {this.state.list.drinks ?  this.state.list.drinks.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> Alcohol </h1>
+                  {this.state.list.alcohol ?  this.state.list.alcohol.map((item) => <p>{item}</p>) : null}
+                </div>
+                <div className='category'>
+                  <h1> General </h1>
+                  {this.state.list.general ?  this.state.list.general.map((item) => <p>{item}</p>) : null}
+                </div>
+            </div>
       </div>
     )
   }
